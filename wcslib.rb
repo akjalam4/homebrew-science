428cf280b4bdde5f26d3d42ec8f4a22f088f8978
akjalam4 <pierre.hallsten@yahoo.com>
2013-04-14 01:51:29 +0300

    Update wcslib.rb

--- wcslib.rb
+++ wcslib.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Wcslib < Formula
   homepage 'http://www.atnf.csiro.au/people/mcalabre/WCS/'
-  url 'ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-4.16.tar.bz2'
+  url 'ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-4.17.tar.bz2'
   sha1 'd321ee3c82ecb14e447006ed475f3f0217aa589f'
 
   option 'with-pgsbox', 'Build PGSBOX, a general curvilinear axis drawing routine for PGPLOT'
